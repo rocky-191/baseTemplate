f2e58ebb93fe456e7a29e4907917456f7b58866c
rocky-191 <1375022007@qq.com>
2016-08-17 16:16:53 +0800

    selectTemplate.html

---
+++ style/js/page/myjs.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){	
+	/**下拉选择***/
+	$(".js-example-basic-hide-search").select2({
+	  minimumResultsForSearch: Infinity
+  	});
+  	$('.ui-select').ui_select();
+	//新增故事
+});

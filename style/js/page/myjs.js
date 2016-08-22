5e28997ad9a6b8b35932784c7e15980f716b58ab
rocky-191 <1375022007@qq.com>
2016-08-22 18:16:09 +0800

    add some pages

--- style/js/page/myjs.js
+++ style/js/page/myjs.js
@@ -3,6 +3,22 @@ $(document).ready(function(){
 	$(".js-example-basic-hide-search").select2({
 	  minimumResultsForSearch: Infinity
   	});
-  	$('.ui-select').ui_select();
 	//新增故事
+  	$('.ui-select').ui_select();
+	
+	var v='1,2,3'//这个为保存的值，自己从数据库读取来赋值给v变量
+	v=','+v+',';//添加分隔符号，好indexOf进行比较
+	var arr=v.split(',');
+	$('#ddlConditions option').each(function(){
+	  if(v.indexOf(','+this.value+',')!=-1)
+	  	this.selected=true;
+	});
+	$("#ddlConditions").multiselect({
+		checkAllText: "全选", 
+		uncheckAllText: '全不选',
+		WinClose : function (val) { 
+            //alert("Values:" + val.MyValues() + " Texts:" + val.MyTexts()); 
+        } 
+	});
+	
 });

7191bfcba3d2434cfa9770875da98b4814c1d910
rocky-191 <1375022007@qq.com>
2016-08-23 09:34:54 +0800

    edit selecTemplate

--- style/js/page/myjs.js
+++ style/js/page/myjs.js
@@ -6,14 +6,15 @@ $(document).ready(function(){
 	//新增故事
   	$('.ui-select').ui_select();
 	
-	var v='1,2,3'//这个为保存的值，自己从数据库读取来赋值给v变量
-	v=','+v+',';//添加分隔符号，好indexOf进行比较
-	var arr=v.split(',');
-	$('#ddlConditions option').each(function(){
-	  if(v.indexOf(','+this.value+',')!=-1)
-	  	this.selected=true;
-	});
+//	var v='1,2,3'//这个为保存的值，自己从数据库读取来赋值给v变量
+//	v=','+v+',';//添加分隔符号，好indexOf进行比较
+//	var arr=v.split(',');
+//	$('#ddlConditions option').each(function(){
+//	  if(v.indexOf(','+this.value+',')!=-1)
+//	  	this.selected=true;
+//	});
 	$("#ddlConditions").multiselect({
+		noneSelectedText: "==请选择==",
 		checkAllText: "全选", 
 		uncheckAllText: '全不选',
 		WinClose : function (val) { 

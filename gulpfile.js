310cf1ca7d5138b3535c1a7ebf36b75fcf692808
rocky-191 <1375022007@qq.com>
2016-11-24 09:57:11 +0800

    add some gulp plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,72 @@
 //导入工具包 require('node_modules里对应模块')
 var gulp = require('gulp'), //本地安装gulp所用到的地方
     less = require('gulp-less'),
+    uglify = require('gulp-uglify'),//压缩js
+    autoprefixer = require('gulp-autoprefixer'),//自动添加前缀
+    cssmin = require('gulp-clean-css'),//压缩css
     livereload = require('gulp-livereload');
 
 //定义一个testless任务
 gulp.task('testless', function() {
     gulp.src('style/css/page/less/*.less')
         .pipe(less())
-        .pipe(gulp.dest('style/css/page/less'))
+        .pipe(gulp.dest('style/css/page'))
+        .pipe(livereload());
+});
+
+//定义压缩js文件任务
+gulp.task('jsmin', function () {
+    gulp.src('style/js/page/*.js')//指定压缩文件
+        .pipe(uglify())//压缩选项配置
+        .pipe(gulp.dest('style/js/distJS'));//制定压缩后的文件位置
+});
+
+//定义压缩css文件任务
+gulp.task('cssmin', function () {
+    gulp.src('style/css/page/*.css')
+        .pipe(cssmin({
+        	advanced: false,//类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+            compatibility: 'ie7',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+            keepBreaks: true,//类型：Boolean 默认：false [是否保留换行]
+            keepSpecialComments: '*'
+            //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+        }))
+        .pipe(gulp.dest('style/css/distCss'));
+});
+
+var browserOptions = {
+  browsers: [
+    'last 3 versions',
+    'ie >= 6',
+    'firefox >= 30',
+    'chrome >= 34',
+    'safari >= 6',
+    'opera >= 12.1',
+    'ios >= 6',
+    'android >= 2.3',
+    'and_uc 9.9',
+  ]
+};
+
+gulp.task('testAutoFx', function () {
+    gulp.src('style/css/less/*.less')
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions',
+					    'ie >= 6',
+					    'firefox >= 30',
+					    'chrome >= 34',
+					    'safari >= 6',
+					    'opera >= 12.1',
+					    'ios >= 6',
+					    'android >= 2.3',
+					    'UCAndroid>=9.9'
+					],
+            cascade: true, //是否美化属性值 默认：true 像这样：
+            //-webkit-transform: rotate(45deg);
+            //        transform: rotate(45deg);
+            remove:true //是否去掉不必要的前缀 默认：true 
+        }))
+        .pipe(gulp.dest('style/css/less'))
         .pipe(livereload());
 });
 
@@ -54,7 +113,7 @@ gulp.task('watch3', function() {
 });
 
  
-gulp.task('default',['testCss','testHtml','testJs','watch','watch1','watch2','watch3']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+gulp.task('default',['testAutoFx','testCss','testHtml','testJs','watch','watch1','watch2','watch3']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
  
 //gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
 //gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组) 

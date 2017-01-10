04de3c1c0f2a317d0961c95d0993534fc69b460d
rocky191 <1375022007@qq.com>
2017-01-10 11:17:42 +0800

    modify usefunction.js

--- style/js/common/usefunction.js
+++ style/js/common/usefunction.js
@@ -876,3 +876,41 @@ for(var i = 0 ; i < parseInt(str.length / 3) ; i++){
 	result.push(s);
 }
 alert( result.join(",") );*/
+
+
+//判断数据类型
+var type = function (o){
+  var s = Object.prototype.toString.call(o);
+  return s.match(/\[object (.*?)\]/)[1].toLowerCase();
+};
+
+type({}); // "object"
+type([]); // "array"
+type(5); // "number"
+type(null); // "null"
+type(); // "undefined"
+type(/abcd/); // "regex"
+type(new Date()); // "date"
+
+//特定数据类型判断
+var myArray=['Null',
+ 'Undefined',
+ 'Object',
+ 'Array',
+ 'String',
+ 'Number',
+ 'Boolean',
+ 'Function',
+ 'RegExp',
+ 'NaN',
+ 'Infinite'
+];
+myArray.forEach(function (t) {
+  type['is' + t] = function (o) {
+    return type(o) === t.toLowerCase();
+  };
+});
+
+type.isObject({}) // true
+type.isNumber(NaN) // true
+type.isRegExp(/abc/) // true

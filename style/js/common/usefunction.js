4d8f61cb3ad70a3c62f12620edf1763096ac8539
rocky191 <1375022007@qq.com>
2017-01-18 16:42:27 +0800

    modify usefunction.js

--- style/js/common/usefunction.js
+++ style/js/common/usefunction.js
@@ -914,3 +914,20 @@ myArray.forEach(function (t) {
 type.isObject({}) // true
 type.isNumber(NaN) // true
 type.isRegExp(/abc/) // true
+
+//对象拷贝方法
+function copyObject(orig) {
+  var copy = Object.create(Object.getPrototypeOf(orig));
+  copyOwnPropertiesFrom(copy, orig);
+  return copy;
+}
+
+function copyOwnPropertiesFrom(target, source) {
+  Object
+  .getOwnPropertyNames(source)
+  .forEach(function(propKey) {
+    var desc = Object.getOwnPropertyDescriptor(source, propKey);
+    Object.defineProperty(target, propKey, desc);
+  });
+  return target;
+}

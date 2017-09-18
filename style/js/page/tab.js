74bb5afb07066ba19551915602af6e39fe36489a
rocky-191 <1375022007@qq.com>
2017-09-18 11:09:43 +0800

    edit tabDemo.html and css

--- style/js/page/tab.js
+++ style/js/page/tab.js
@@ -27,5 +27,51 @@ window.onload = function(){
             this.className="now";
             Otext[this.id].style.display="block";//通过li的索引来显示和隐藏Otext内容，取出来的div内容是一个数组 
         }                        
-    }       
+    }
+    
+    //类型三点击切换
+    var Obox1 = document.getElementById("box2");
+	var Oli1 =Obox1.getElementsByTagName("li");
+	var Olist1 =document.getElementById("list2");
+	var Otext1 =Olist1.getElementsByTagName("div");
+    for(var i=0,len=Oli1.length; i<len;i++){
+        Oli1[i].id=i;//索引
+        Oli1[i].onclick =function(){                                
+            for(var i=0,len1=Oli.length; i<len1;i++){
+                Oli1[i].className="";
+                Otext1[i].style.display="none";                               
+            }
+            this.className="now";
+            Otext1[this.id].style.display="block";//通过li的索引来显示和隐藏Otext内容，取出来的div内容是一个数组 
+        }                        
+    }
+    
+    //类型四
+    var OneLi=document.getElementsByClassName('newbox')[0].getElementsByTagName('li');
+	var TwoLi=document.getElementsByClassName('boxtwo')[0].getElementsByTagName('li');
+	var a=document.getElementsByClassName('newbox')[0].getElementsByTagName('a')
+	var timer=null;
+	var num=0;
+	OneLi[0].className='bj'
+	a[0].style.color='white'
+		
+	showtimer()
+	
+	function showtimer(){
+		timer=setInterval(function(){
+			num++;
+			if(num>=OneLi.length){
+				num=0
+			}
+			for(var j=0;j<OneLi.length;j++){
+				OneLi[j].className=''
+				TwoLi[j].className='hide'
+				a[j].style.color=''
+			}
+			OneLi[num].className='bj'
+			TwoLi[num].className='show'
+			a[num].style.color='white'
+			
+		},3000)
+	}
 }

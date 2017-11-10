51b35037240ef887cc48f3db676c0c25d0420bf3
rocky-191 <1375022007@qq.com>
2017-11-10 16:06:51 +0800

    edit checkboxAndRadio.html

--- style/js/common/checkboxModal.js
+++ style/js/common/checkboxModal.js
@@ -1,4 +1,4 @@
-function checkbox_check(event){
+/*function checkbox_check(event){
 	var ev = event || window.event;
     var target = ev.target || ev.srcElement;
 		if(!$(target).parent().hasClass("on_check")){
@@ -43,4 +43,54 @@ function btn_delete(id,checkboxName){
 	}else{
 		alert("请选择一条数据！");
 	}	
+}*/
+
+//方法二
+function checkbox_check(event,checkBoxName){	
+	var ev = event || window.event;
+    var target = ev.target || ev.srcElement;
+	if(!$(target).parent().hasClass("on_check")){
+		$(target).attr("checked","checked");
+		$(target).parent().addClass("on_check");
+		
+		//是否选中全选
+		var check =window.document.getElementsByName(checkBoxName);
+		var checkNum=0;
+		$("input[type='checkbox'][name='"+checkBoxName+"']").each(function(){
+	 		if($(this).parent().hasClass("on_check")){
+	 			checkNum++
+	 		}
+	 	});
+	 	if(check.length==checkNum){
+	 		$(".js-checkAll").addClass("on_check");
+			$(".js-checkAll input").attr("checked","checked");
+	 	}
+		
+	}else{
+		$(target)[0].removeAttribute("checked");
+		$(target).parent().removeClass("on_check");
+		
+		$(".js-checkAll").removeClass("on_check");
+		$(".js-checkAll input")[0].removeAttribute("checked");
+	}    	
+}
+
+function formwork_checkbox_checkAll(event,checkBoxName){
+	var ev = event || window.event;
+    var target = ev.target || ev.srcElement;
+	var chk =window.document.getElementsByName(checkBoxName);
+	var l=chk.length;
+	if(!$(target).parent().hasClass("on_check")){
+		$(target).parent().addClass("on_check");
+		for (var i = 0; i <l ; i++) {
+	  		$(chk[i].parentElement).addClass("on_check");
+	  		$(chk[i]).attr("checked","checked");
+	 	}		
+	}else{
+		$(target).parent().removeClass("on_check");
+		for (var i = 0; i <l; i++) {
+	  		$(chk[i].parentElement).removeClass("on_check");
+	  		$(chk[i])[0].removeAttribute("checked");
+	 	}
+	}
 }

be3faf1ad4564faf85ab09aafe3103806d3c4dd3
rocky-191 <1375022007@qq.com>
2017-12-08 11:23:29 +0800

    edit common.js add debounce function

--- style/js/common/common.js
+++ style/js/common/common.js
@@ -38,3 +38,21 @@ function getStyle(element,attr){
 		return window.getComputedStyle(element,null)[attr];
 	}
 }
+
+// debounce函数用来包裹我们的事件
+function debounce(fn, delay) {
+  // 持久化一个定时器 timer
+  let timer = null;
+  // 闭包函数可以访问 timer
+  return function() {
+    // 通过 'this' 和 'arguments'
+    // 获得函数的作用域和参数
+    let context = this;
+    let args = arguments;
+    // 如果事件被触发，清除 timer 并重新开始计时
+    clearTimeout(timer);
+    timer = setTimeout(function() {
+      fn.apply(context, args);
+    }, delay);
+  }
+}

984eaeaf65392f09395c2e493496b9629e94758b
rocky-191 <1375022007@qq.com>
2017-12-12 17:08:41 +0800

    edit common.js

--- style/js/common/common.js
+++ style/js/common/common.js
@@ -56,3 +56,37 @@ function debounce(fn, delay) {
     }, delay);
   }
 }
+
+//数组随机排序算法
+//方法一：
+//var arr = [1,2,3,4,5,6,7,8,9,10];
+function randSort1(arr){
+	for(var i = 0,len = arr.length;i < len; i++ ){
+		var rand = parseInt(Math.random()*len);
+		var temp = arr[rand];
+		arr[rand] = arr[i];
+		arr[i] = temp;
+	}
+	return arr;
+}
+//console.log(randSort1(arr));
+  	
+//方法二：
+//var arr = [1,2,3,4,5,6,7,8,9,10];
+function randSort2(arr){
+	var mixedArray = [];
+	while(arr.length > 0){
+		var randomIndex = parseInt(Math.random()*arr.length);
+		mixedArray.push(arr[randomIndex]);
+		arr.splice(randomIndex, 1);
+	}
+	return mixedArray;
+}
+//console.log(randSort2(arr));
+
+//方法三：
+//var arr = [1,2,3,4,5,6,7,8,9,10];
+//arr.sort(function(){
+//	return Math.random() - 0.5;
+//})
+//console.log(arr);

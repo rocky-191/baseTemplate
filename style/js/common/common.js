5d8f29f03a7ee04b09286fc923f25953d65fc582
rocky-191 <1375022007@qq.com>
2017-12-22 17:57:12 +0800

    modify common.js

--- style/js/common/common.js
+++ style/js/common/common.js
@@ -173,3 +173,38 @@ function play() {
     },500);
 }
 play();
+
+/**
+ * 判断一个对象是否是数组，参数不是对象或者不是数组，返回false
+ *
+ * @param {Object} arg 需要测试是否为数组的对象
+ * @return {Boolean} 传入参数是数组返回true，否则返回false
+ * 
+ * author:https://github.com/qiu-deqing
+ */
+function isArray(arg) {
+    if (typeof arg === 'object') {
+        return Object.prototype.toString.call(arg) === '[object Array]';
+    }
+    return false;
+}
+
+/**
+ * 判断对象是否为函数，如果当前运行环境对可调用对象（如正则表达式）
+ * 的typeof返回'function'，采用通用方法，否则采用优化方法
+ *
+ * @param {Any} arg 需要检测是否为函数的对象
+ * @return {boolean} 如果参数是函数，返回true，否则false
+ * 
+ * author:https://github.com/qiu-deqing
+ */
+function isFunction(arg) {
+    if (arg) {
+        if (typeof (/./) !== 'function') {
+            return typeof arg === 'function';
+        } else {
+            return Object.prototype.toString.call(arg) === '[object Function]';
+        }
+    } // end if
+    return false;
+}
